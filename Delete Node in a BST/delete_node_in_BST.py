fd06013edd8104e0b54fd1c87beba6ea12dfbbd9
dianalyzenko <lyzenko.pn@ucu.edu.ua>
2025-05-02 18:02:44 +0300

    Add files via upload

---
+++ Delete Node in a BST/delete_node_in_BST.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            successor = self.get_successor(root.right)
+            root.val = successor.val
+            root.right = self.deleteNode(root.right, successor.val)
+        return root
+    
+    def get_successor(self, node):
+        current = node
+        while current.left:
+            current = current.left
+        return current
